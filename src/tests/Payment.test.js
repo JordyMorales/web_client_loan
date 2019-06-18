384cde3cb77fe36c131d2cec441a9ef557ea33c5
Jordy Morales <jomora24@gmail.com>
2019-06-17 20:14:13 -0400

    Tests were implemented for some components

--- src/tests/Payment.test.js
+++ src/tests/Payment.test.js
@@ -2,13 +2,13 @@ import React from "react";
 import { configure } from "enzyme";
 import { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import NewCustomer from "../components/NewCustomer/NewCustomer";
+import Payment from "../components/Payment/Payment";
 
 configure({ adapter: new Adapter() });
 
 describe("Save Button", () => {
   test("should be disabled when email and amount are not passed in", () => {
-    const wrapper = shallow(<NewCustomer values={{ email: "", amount: "" }} />);
-    expect(wrapper.find('Button[content="Save"]').prop("disabled")).toBe(true);
+    const wrapper = shallow(<Payment />);
+    expect(wrapper.find('Button[content="pay"]').prop("disabled")).toBe(true);
   });
 });
